5a7d87bf3990736c19687c772571c4bd03d2006b
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2021-01-10 17:42:41 +0100

    Update PitchTile.cs

--- Assets/Scripts/Match Scene/Pitch/PitchTile.cs
+++ Assets/Scripts/Match Scene/Pitch/PitchTile.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PitchTile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /* #region ==== FIELDS & PROPERTIES ======================================================= */
+    
+    /* #region ---- Dependencies -------------------------------------------------------------- */
+    private MatchManager MatchManager;
+
+    /* #endregion */
+    /* ---------------------------------------------------------------------------------------- */
+
+    /* #endregion */
+    /* ======================================================================================== */
+
+    /* #region ==== GET DEPENDENCIES ========================================================== */
+    private void getDependencies()
+    {
+        getMatchManager();
+    }
+
+    /* #region ---- Get MatchManager ---------------------------------------------------------- */
+    public void getMatchManager()
+    {
+        MatchManager = MatchManager.Instance;
+    }
+    /* #endregion */
+    /* ---------------------------------------------------------------------------------------- */
+
+    /* #endregion */
+    /* ======================================================================================== */
+
+    /* #region ==== AWAKE / START ============================================================= */
+    void Awake() 
+    {
+        getDependencies();
+    }
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /* #endregion */
+    /* ======================================================================================== */
+
 }
